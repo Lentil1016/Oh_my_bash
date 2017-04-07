c6999bf2d7d4f141ff218d9660c3d9437e5cf849
lentil1016 <lentil1016@gmail.com>
2017-04-07 15:18:59 +0800

    add usage text

--- Logo/logo.sh
+++ Logo/logo.sh
@@ -1,83 +1,94 @@
 #!/bin/bash
 
-LOGO_FILE="/logo/${1}_logo"
+if [ $# -eq 1 -a -e `pwd`/logo/${1}_logo ];
+then
+	LOGO_FILE="`pwd`/logo/${1}_logo"
 
-MSG_LINES=(
-""
-""
-""
-"Developer"
-"Email"
-"Date"
-""
-"OS"
-"Kernel"
-"Host"
-"User"
-""
-"CC"
-"Boost"
-"Lua"
-"Haskell"
-)
+	MSG_LINES=(
+	""
+	""
+	""
+	"Developer"
+	"Email"
+	"Date"
+	""
+	"OS"
+	"Kernel"
+	"Host"
+	"User"
+	""
+	"CC"
+	"Boost"
+	"Lua"
+	"Haskell"
+	)
 
 
-DEVELOPER='Lentil Sun'
-EMAIL="t_sunyk@channelsoft.com"
-DATE=`date +%Y-%m-%d`
+	DEVELOPER='Lentil Sun'
+	EMAIL="t_sunyk@channelsoft.com"
+	DATE=`date +%Y-%m-%d`
 
-OS=`head -n 1 /etc/issue`
-KERNEL="`uname -s` `uname -r`"
-HOST=`hostname`
-USER=`whoami`
+	OS=`head -n 1 /etc/issue`
+	KERNEL="`uname -s` `uname -r`"
+	HOST=`hostname`
+	USER=`whoami`
 
-CC=`g++ --version|grep g++|awk '{print $2,$3}'`
-BOOST=`cat /usr/include/boost/version.hpp|grep '#define'|grep LIB_VERSION|awk '{print $3}'|sed 's/\"//g'|sed 's/\_/./g'`
-LUA=`lua -v 2>&1|awk '{print $2}'`
-HASKELL=`ghc --version|awk '{print $8}'`
+	CC=`g++ --version|grep g++|awk '{print $2,$3}'`
+	BOOST=`cat /usr/include/boost/version.hpp|grep '#define'|grep LIB_VERSION|awk '{print $3}'|sed 's/\"//g'|sed 's/\_/./g'`
+	LUA=`lua -v 2>&1|awk '{print $2}'`
+	HASKELL=`ghc --version|awk '{print $8}'`
 
-tput init
-IFS="|"
+	tput init
+	IFS="|"
 
-BL="$(tput setaf 4)"
-DBL="$(tput setaf 33)"
-SBL="$(tput setaf 12)"
-OR="$(tput setaf 214)"
-RE="$(tput setaf 7)"
-DOR="$(tput setaf 196)"
-SOR="$(tput setaf 226)"
-
-tput bold;
-
-function vecho()
-{
+	BL="$(tput setaf 4)"
+	DBL="$(tput setaf 33)"
+	SBL="$(tput setaf 12)"
+	OR="$(tput setaf 214)"
 	RE="$(tput setaf 7)"
-	YE="$(tput setaf 11)"
-	GR="$(tput setaf 250)"
-		
-	if [ -n "$1" ] ;
-	then
-		eval value=\$"$(echo $1|tr '[:lower:]' '[:upper:]')"
-		if [ -n "$value" ] ;
+	DOR="$(tput setaf 196)"
+	SOR="$(tput setaf 226)"
+
+	tput bold;
+
+	function vecho()
+	{
+		RE="$(tput setaf 7)"
+		YE="$(tput setaf 11)"
+		GR="$(tput setaf 250)"
+			
+		if [ -n "$1" ] ;
 		then
-			echo ${YE}$1: ${GR}$value${RE}
+			eval value=\$"$(echo $1|tr '[:lower:]' '[:upper:]')"
+			if [ -n "$value" ] ;
+			then
+				echo ${YE}$1: ${GR}$value${RE}
+			else
+				echo ${RED}$1: ${GR}Not found${RE}
+			fi
 		else
-			echo ${RED}$1: ${GR}Not found${RE}
+			echo
 		fi
-	else
-		echo
-	fi
-}
+	}
 
-i=0
-echo
-while read line
-do
-	eval echo -n $line
-	vecho ${MSG_LINES[$i]}
-	let i+=1
-done < $LOGO_FILE
+	i=0
+	echo
+	while read line
+	do
+		eval echo -n $line
+		vecho ${MSG_LINES[$i]}
+		let i+=1
+	done < $LOGO_FILE
 
-echo
-tput sgr0
-
+	echo
+	tput sgr0
+else
+	echo "Usage: ${0} logo_name"
+	function list()
+	{
+		echo 'Available logo_name: '
+		ls logo|awk -F "_" '{printf $1 "	"}'
+	}
+	list
+	echo
+fi
